8ebc501e3685bb123c4ce14f2cd3e425f08ee56d
unknown <kimjiyoon0728@naver.com>
2023-04-03 15:24:22 +0900

    http_app완료

---
+++ test/post_repository_test.dart
@@ -0,0 +1,28 @@
+import 'package:http_app1/post_repository.dart';
+
+void main() async {
+  await findById_test();
+}
+
+
+Future<void> findAll_test() async{
+  //given
+
+  //when
+  PostRepository postRepository = PostRepository();
+  await postRepository.findAll();
+
+
+  //then
+}
+Future<void> findById_test() async{
+  //given
+  int id=1;
+
+  //when
+  PostRepository postRepository = PostRepository();
+  await postRepository.findById(id);
+
+
+  //then
+}
